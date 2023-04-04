093f73901ffed09453efd356714a1236d5469d81
stoyan stoyanov - stucky <stoyan.v.stoyanov86@gmail.com>
2023-04-04 19:05:32 +0300

    Add project files.

---
+++ Journey-Models/Enums/HexType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey_Models.Enums
+{
+    public enum HexType
+    {
+        Empty = 0,
+        Unknown = 1,
+        Rock = 2,
+        Tree = 3,
+        Water = 4,
+    }
+}
---
+++ Journey-Models/Models/Hex.cs
@@ -0,0 +1,11 @@
+﻿using Journey_Models.Enums;
+
+namespace Journey_Models.Models
+{
+    public class Hex
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public HexType Type { get; set; }
+    }
+}
